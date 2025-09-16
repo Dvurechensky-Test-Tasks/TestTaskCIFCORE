1f7377f58efb1efa6d6a227e9f661977f17bd69e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-16 06:51:30 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-16

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 сентября 2025 06:50:28
- * Version: 1.0.24
+ * Last Updated: 16 сентября 2025 06:50:28
+ * Version: 1.0.25
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Binding/Binders/ScopeConcreteIdArgConditionCopyNonLazyBinder.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Binding/Binders/ScopeConcreteIdArgConditionCopyNonLazyBinder.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 15 сентября 2025 06:50:28
- * Version: 1.0.24
+ * Last Updated: 16 сентября 2025 06:50:28
+ * Version: 1.0.25
  */
 
 namespace Zenject
