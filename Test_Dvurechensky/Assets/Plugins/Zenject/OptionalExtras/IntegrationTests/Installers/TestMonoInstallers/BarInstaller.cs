b69ce181bd8b48357960e4500dbae229a66fa6fe
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-24 06:51:31 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-24

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestMonoInstallers/BarInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestMonoInstallers/BarInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 сентября 2025 17:13:59
- * Version: 1.0.32
+ * Last Updated: 24 сентября 2025 06:50:28
+ * Version: 1.0.33
  */
 
 namespace Zenject.Tests.Installers.MonoInstallers
