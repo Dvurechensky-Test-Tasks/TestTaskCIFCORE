c3ef8802e4eb68b05b459119a7fff560878bf9fc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-09-30 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2025-09-30

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 сентября 2025 06:50:28
- * Version: 1.0.38
+ * Last Updated: 30 сентября 2025 06:50:27
+ * Version: 1.0.39
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/ReflectionBaking/Unity/ZenjectReflectionBakingSettingsEditor.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/ReflectionBaking/Unity/ZenjectReflectionBakingSettingsEditor.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 сентября 2025 06:50:28
- * Version: 1.0.38
+ * Last Updated: 30 сентября 2025 06:50:27
+ * Version: 1.0.39
  */
 
 using UnityEditor;
