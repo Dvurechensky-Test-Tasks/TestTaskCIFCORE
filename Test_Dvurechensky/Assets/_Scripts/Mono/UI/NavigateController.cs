a1efd8f6b4375e6337b0f39fa79683393eda51bf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-02 06:51:29 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-02

--- Test_Dvurechensky/Assets/_Scripts/Mono/UI/NavigateController.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/UI/NavigateController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 октября 2025 06:50:28
- * Version: 1.0.40
+ * Last Updated: 02 октября 2025 06:50:27
+ * Version: 1.0.41
  */
 
 using System.ComponentModel;
