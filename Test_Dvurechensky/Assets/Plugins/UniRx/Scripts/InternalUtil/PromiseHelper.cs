bdcf708d6efb9a214e2c1252a45df146585b23e8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-05 06:51:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-05

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/PromiseHelper.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/PromiseHelper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 октября 2025 06:50:28
- * Version: 1.0.43
+ * Last Updated: 05 октября 2025 06:50:27
+ * Version: 1.0.44
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
