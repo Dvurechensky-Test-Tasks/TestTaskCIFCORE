9f9c25f0277b9ad2f713a8ba60e47e9c1bb4dd35
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-08 06:51:31 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-08

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 октября 2025 08:37:12
- * Version: 1.0.46
+ * Last Updated: 08 октября 2025 06:50:28
+ * Version: 1.0.47
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneTests/TestDestructionOrder/Scene1Installer.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneTests/TestDestructionOrder/Scene1Installer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 07 октября 2025 08:37:12
- * Version: 1.0.46
+ * Last Updated: 08 октября 2025 06:50:28
+ * Version: 1.0.47
  */
 
 using UnityEngine.SceneManagement;
