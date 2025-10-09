d747d0340ccf93014dda3633aa75249ec4934ce9
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-09 06:52:34 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-09

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 октября 2025 06:50:28
- * Version: 1.0.47
+ * Last Updated: 09 октября 2025 06:51:38
+ * Version: 1.0.48
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestMonoInstallers/BarInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestMonoInstallers/BarInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 октября 2025 06:50:28
- * Version: 1.0.47
+ * Last Updated: 09 октября 2025 06:51:38
+ * Version: 1.0.48
  */
 
 namespace Zenject.Tests.Installers.MonoInstallers
