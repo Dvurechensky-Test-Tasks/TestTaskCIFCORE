ac083889b17c36ef8a31e05365c638f1fe26efd4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-13 10:01:46 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-13

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Disposables/CancellationDisposable.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Disposables/CancellationDisposable.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 октября 2025 10:57:33
- * Version: 1.0.51
+ * Last Updated: 13 октября 2025 10:00:48
+ * Version: 1.0.52
  */
 
 // original code from GitHub Reactive-Extensions/Rx.NET
