a20ae4e9276453873f3d7c639f882f3007d70479
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-10-26 12:23:52 +0300

    Auto-update headers via pre_commit.ps1 - 2025-10-26

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/System/IProgress.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/System/IProgress.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 октября 2025 06:50:28
- * Version: 1.0.65
+ * Last Updated: 26 октября 2025 12:22:53
+ * Version: 1.0.66
  */
 
 // defined from .NET Framework 4.5 and NETFX_CORE
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/Decorators/Scene1Installer.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/Decorators/Scene1Installer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 25 октября 2025 06:50:28
- * Version: 1.0.65
+ * Last Updated: 26 октября 2025 12:22:53
+ * Version: 1.0.66
  */
 
 namespace Zenject.Tests.DecoratorTests
