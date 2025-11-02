540c8d6c70ac74084eb4fdc5e1f35787f76e30df
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-02 17:22:44 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-02

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 ноября 2025 06:50:27
- * Version: 1.0.72
+ * Last Updated: 02 ноября 2025 17:21:45
+ * Version: 1.0.73
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Util/ProfileTimers.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Util/ProfileTimers.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 01 ноября 2025 06:50:27
- * Version: 1.0.72
+ * Last Updated: 02 ноября 2025 17:21:45
+ * Version: 1.0.73
  */
 
 #if ZEN_INTERNAL_PROFILING
