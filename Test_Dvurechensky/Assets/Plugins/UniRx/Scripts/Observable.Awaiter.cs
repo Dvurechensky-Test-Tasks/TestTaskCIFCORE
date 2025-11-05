3ea4e455874b566d71918c736f9eebd17c862421
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-05 06:51:25 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-05

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Observable.Awaiter.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Observable.Awaiter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 ноября 2025 06:50:27
- * Version: 1.0.75
+ * Last Updated: 05 ноября 2025 06:50:27
+ * Version: 1.0.76
  */
 
 #if (NET_4_6 || NET_STANDARD_2_0)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/SampleGame1 (Beginner)/Scripts/Util/GameEvents.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/SampleGame1 (Beginner)/Scripts/Util/GameEvents.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 ноября 2025 06:50:27
- * Version: 1.0.75
+ * Last Updated: 05 ноября 2025 06:50:27
+ * Version: 1.0.76
  */
 
 namespace Zenject.Asteroids
