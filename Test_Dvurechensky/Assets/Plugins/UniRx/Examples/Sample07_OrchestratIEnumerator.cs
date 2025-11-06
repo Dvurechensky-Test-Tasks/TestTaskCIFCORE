418aeb56736d759fc604c559a9eaf042992e8d0e
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-06 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-06

--- Test_Dvurechensky/Assets/Plugins/UniRx/Examples/Sample07_OrchestratIEnumerator.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Examples/Sample07_OrchestratIEnumerator.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 ноября 2025 06:50:27
- * Version: 1.0.76
+ * Last Updated: 06 ноября 2025 06:50:31
+ * Version: 1.0.77
  */
 
 #pragma warning disable 0168
