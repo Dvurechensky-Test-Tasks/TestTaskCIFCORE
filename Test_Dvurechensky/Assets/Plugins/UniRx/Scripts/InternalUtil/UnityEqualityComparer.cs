6b184a3d6e82b3ec660ee76a3ebe63bbf5f71cbb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-09 06:51:24 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-09

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 ноября 2025 06:50:27
- * Version: 1.0.79
+ * Last Updated: 09 ноября 2025 06:50:49
+ * Version: 1.0.80
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/BarInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/BarInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 ноября 2025 06:50:27
- * Version: 1.0.79
+ * Last Updated: 09 ноября 2025 06:50:49
+ * Version: 1.0.80
  */
 
 namespace Zenject.Tests.Installers.ScriptableObjectInstallers
