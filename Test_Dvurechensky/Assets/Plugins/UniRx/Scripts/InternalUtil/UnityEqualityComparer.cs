0f5e3f6647714a2cd593305ce73b1e0b56058bc5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-14 06:51:26 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-14

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 ноября 2025 06:50:27
- * Version: 1.0.84
+ * Last Updated: 14 ноября 2025 06:50:28
+ * Version: 1.0.85
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/_Scripts/Mono/UI/NavigateController.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/UI/NavigateController.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 ноября 2025 06:50:27
- * Version: 1.0.84
+ * Last Updated: 14 ноября 2025 06:50:28
+ * Version: 1.0.85
  */
 
 using System.ComponentModel;
