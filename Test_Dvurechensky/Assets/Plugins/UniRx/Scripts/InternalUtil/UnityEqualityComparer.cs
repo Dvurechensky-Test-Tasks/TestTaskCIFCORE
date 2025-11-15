8621c2949ea9e4e9c6f3b3682f4c25f0f047df19
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-15 06:51:22 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-15

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/UnityEqualityComparer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 ноября 2025 06:50:28
- * Version: 1.0.85
+ * Last Updated: 15 ноября 2025 06:50:28
+ * Version: 1.0.86
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 14 ноября 2025 06:50:28
- * Version: 1.0.85
+ * Last Updated: 15 ноября 2025 06:50:28
+ * Version: 1.0.86
  */
 
 public class ForecastItemFactory
