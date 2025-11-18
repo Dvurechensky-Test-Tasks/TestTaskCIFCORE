2a5af9a8524d81b8aad710d2e5ef6cd8ff4fb43d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-18 06:51:25 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-18

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Factories/TestBindFactoryOne/FooInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Factories/TestBindFactoryOne/FooInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 ноября 2025 06:50:27
- * Version: 1.0.88
+ * Last Updated: 18 ноября 2025 06:50:28
+ * Version: 1.0.89
  */
 
 namespace Zenject.Tests.Factories.BindFactoryOne
