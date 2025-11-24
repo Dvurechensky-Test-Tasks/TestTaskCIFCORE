813156aab809b8954b1f685190334dcc27f1b010
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-24 12:25:56 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-24

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/TestFramework/SceneTestFixture.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/TestFramework/SceneTestFixture.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 23 ноября 2025 06:50:27
- * Version: 1.0.94
+ * Last Updated: 24 ноября 2025 12:24:57
+ * Version: 1.0.95
  */
 
 using System.Collections;
