1904839c2c1bacd181d813dbe48169b8067172dd
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-11-25 06:51:24 +0300

    Auto-update headers via pre_commit.ps1 - 2025-11-25

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/PromiseHelper.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/PromiseHelper.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 ноября 2025 12:24:57
- * Version: 1.0.95
+ * Last Updated: 25 ноября 2025 06:50:27
+ * Version: 1.0.96
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 ноября 2025 12:24:57
- * Version: 1.0.95
+ * Last Updated: 25 ноября 2025 06:50:27
+ * Version: 1.0.96
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
