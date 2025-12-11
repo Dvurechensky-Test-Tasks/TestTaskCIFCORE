1df0d3c9101d1def385cf938e4ae7de9254793a3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-11 14:03:32 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-11

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 декабря 2025 06:50:27
- * Version: 1.0.111
+ * Last Updated: 11 декабря 2025 14:02:32
+ * Version: 1.0.112
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/QuxInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/QuxInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 декабря 2025 06:50:27
- * Version: 1.0.111
+ * Last Updated: 11 декабря 2025 14:02:32
+ * Version: 1.0.112
  */
 
 namespace Zenject.Tests.Installers.ScriptableObjectInstallers
