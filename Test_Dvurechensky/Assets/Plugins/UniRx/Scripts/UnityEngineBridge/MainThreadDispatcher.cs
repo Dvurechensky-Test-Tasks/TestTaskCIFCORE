076d01f73edf4f016e7ebb87c709942d39f67169
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-14 14:33:35 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-14

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 декабря 2025 15:59:51
- * Version: 1.0.114
+ * Last Updated: 14 декабря 2025 14:32:37
+ * Version: 1.0.115
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableTransformChangedTrigger.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableTransformChangedTrigger.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 13 декабря 2025 15:59:51
- * Version: 1.0.114
+ * Last Updated: 14 декабря 2025 14:32:37
+ * Version: 1.0.115
  */
 
 // after uGUI(from 4.6)
