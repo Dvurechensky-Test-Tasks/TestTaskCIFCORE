8dda789742a0893a38d7efec18b1119f862d08da
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-19 06:51:29 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-19

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Tests/Factories/PrefabFactory/TestPrefabFactory.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Tests/Factories/PrefabFactory/TestPrefabFactory.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 декабря 2025 06:50:28
- * Version: 1.0.119
+ * Last Updated: 19 декабря 2025 06:50:29
+ * Version: 1.0.120
  */
 
 using System.Collections;
