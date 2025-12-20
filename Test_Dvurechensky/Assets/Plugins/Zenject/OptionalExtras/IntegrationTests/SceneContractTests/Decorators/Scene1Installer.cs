2ab47f9f179e8d952b5b92d9c9b8eb5b69b209e3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-20 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-20

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/Decorators/Scene1Installer.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/Decorators/Scene1Installer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 19 декабря 2025 06:50:29
- * Version: 1.0.120
+ * Last Updated: 20 декабря 2025 06:50:28
+ * Version: 1.0.121
  */
 
 namespace Zenject.Tests.DecoratorTests
