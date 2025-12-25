7f5fac951422fa8736bb8bfcdddab925267ec172
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-25 09:30:10 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-25

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Toolkit/ObjectPool.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Toolkit/ObjectPool.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 декабря 2025 09:27:48
- * Version: 1.0.125
+ * Last Updated: 25 декабря 2025 09:29:10
+ * Version: 1.0.126
  */
 
 #if UNITY_5_3_OR_NEWER
