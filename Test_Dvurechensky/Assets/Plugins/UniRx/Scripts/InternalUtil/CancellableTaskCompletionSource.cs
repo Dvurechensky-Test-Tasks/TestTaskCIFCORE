117ad719b6bbe6abe54554b8332c1f7ec1b81c5f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2025-12-28 06:51:27 +0300

    Auto-update headers via pre_commit.ps1 - 2025-12-28

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/CancellableTaskCompletionSource.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/CancellableTaskCompletionSource.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 декабря 2025 15:50:16
- * Version: 1.0.128
+ * Last Updated: 28 декабря 2025 06:50:28
+ * Version: 1.0.129
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Schedulers/ThreadPoolScheduler.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Schedulers/ThreadPoolScheduler.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 27 декабря 2025 15:50:16
- * Version: 1.0.128
+ * Last Updated: 28 декабря 2025 06:50:28
+ * Version: 1.0.129
  */
 
 #if !UNITY_METRO
