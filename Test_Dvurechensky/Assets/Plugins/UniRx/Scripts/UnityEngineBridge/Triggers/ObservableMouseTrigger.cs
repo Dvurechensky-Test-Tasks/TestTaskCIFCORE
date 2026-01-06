fc39506bd0e6a86fafa6c34390980428ca4f54ac
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-06 06:51:29 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-06

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableMouseTrigger.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Triggers/ObservableMouseTrigger.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 05 января 2026 15:45:21
- * Version: 1.0.137
+ * Last Updated: 06 января 2026 06:50:29
+ * Version: 1.0.138
  */
 
 #if !(UNITY_IPHONE || UNITY_ANDROID || UNITY_METRO)
