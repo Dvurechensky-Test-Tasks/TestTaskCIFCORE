532b482e3c262548ab21c7cf31d79e4d638ba5d0
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-09 16:10:28 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-09

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 января 2026 11:23:27
- * Version: 1.0.140
+ * Last Updated: 09 января 2026 16:09:29
+ * Version: 1.0.141
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Providers/PrefabCreators/IPrefabInstantiator.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Providers/PrefabCreators/IPrefabInstantiator.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 08 января 2026 11:23:27
- * Version: 1.0.140
+ * Last Updated: 09 января 2026 16:09:29
+ * Version: 1.0.141
  */
 
 #if !NOT_UNITY3D
