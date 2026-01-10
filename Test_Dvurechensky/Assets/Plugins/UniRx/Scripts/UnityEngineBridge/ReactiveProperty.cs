9217a5d4039fdea2b841655e47bf4947c0125522
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-10 06:51:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-10

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ReactiveProperty.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/ReactiveProperty.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 января 2026 16:09:29
- * Version: 1.0.141
+ * Last Updated: 10 января 2026 06:50:28
+ * Version: 1.0.142
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Util/ReflectionInfoTypeInfoConverter.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Util/ReflectionInfoTypeInfoConverter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 января 2026 16:09:29
- * Version: 1.0.141
+ * Last Updated: 10 января 2026 06:50:28
+ * Version: 1.0.142
  */
 
 //#define ZEN_DO_NOT_USE_COMPILED_EXPRESSIONS
