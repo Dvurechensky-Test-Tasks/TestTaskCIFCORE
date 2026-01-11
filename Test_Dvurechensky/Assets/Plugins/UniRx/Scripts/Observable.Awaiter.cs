61bb5ecc30dba8cf1ab75c6ff1625021517efbae
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-11 17:08:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-11

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Observable.Awaiter.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Observable.Awaiter.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 января 2026 06:50:28
- * Version: 1.0.142
+ * Last Updated: 11 января 2026 17:07:49
+ * Version: 1.0.143
  */
 
 #if (NET_4_6 || NET_STANDARD_2_0)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/BarInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Installers/TestScriptableObjectInstallers/BarInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 января 2026 06:50:28
- * Version: 1.0.142
+ * Last Updated: 11 января 2026 17:07:49
+ * Version: 1.0.143
  */
 
 namespace Zenject.Tests.Installers.ScriptableObjectInstallers
