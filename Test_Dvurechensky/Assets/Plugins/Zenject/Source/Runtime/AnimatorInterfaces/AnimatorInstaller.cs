15e82d44b53331213d06781e363ee20d9ab79955
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-12 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-12

--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Runtime/AnimatorInterfaces/AnimatorInstaller.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Runtime/AnimatorInterfaces/AnimatorInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 11 января 2026 17:07:49
- * Version: 1.0.143
+ * Last Updated: 12 января 2026 06:50:28
+ * Version: 1.0.144
  */
 
 using UnityEngine;
