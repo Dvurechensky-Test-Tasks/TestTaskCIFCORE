7576c350fc66ad363454e7963b588b20b51a8df4
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-18 15:17:44 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-18

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityWinRTBridge/ThreadPoolScheduler_UnityWinRT.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityWinRTBridge/ThreadPoolScheduler_UnityWinRT.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 января 2026 11:55:40
- * Version: 1.0.149
+ * Last Updated: 18 января 2026 15:16:45
+ * Version: 1.0.150
  */
 
 #if UNITY_METRO
