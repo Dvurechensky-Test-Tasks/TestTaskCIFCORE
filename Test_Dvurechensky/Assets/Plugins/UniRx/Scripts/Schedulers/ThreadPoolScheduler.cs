1e363c466600fa5b509e090a7ab6a8f73402f319
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-21 06:51:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-21

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Schedulers/ThreadPoolScheduler.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Schedulers/ThreadPoolScheduler.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 января 2026 06:50:28
- * Version: 1.0.152
+ * Last Updated: 21 января 2026 06:50:28
+ * Version: 1.0.153
  */
 
 #if !UNITY_METRO
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/MainThreadDispatcher.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 20 января 2026 06:50:28
- * Version: 1.0.152
+ * Last Updated: 21 января 2026 06:50:28
+ * Version: 1.0.153
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
