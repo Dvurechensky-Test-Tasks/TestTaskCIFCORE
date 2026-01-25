4b543f2e25240d9cbf1568fe368067b193fd89f5
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-25 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-25

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 января 2026 09:16:42
- * Version: 1.0.156
+ * Last Updated: 25 января 2026 06:50:28
+ * Version: 1.0.157
  */
 
 #if !(NETFX_CORE || NET_4_6 || NET_STANDARD_2_0 || UNITY_WSA_10_0)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Bindings/TestFromSubContainerPrefabResource/Gorp.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Bindings/TestFromSubContainerPrefabResource/Gorp.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 24 января 2026 09:16:42
- * Version: 1.0.156
+ * Last Updated: 25 января 2026 06:50:28
+ * Version: 1.0.157
  */
 
 namespace Zenject.Tests.Bindings.FromSubContainerPrefabResource
