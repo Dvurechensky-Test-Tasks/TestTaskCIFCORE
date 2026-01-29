7b0ba885797d41245cfa07ed1f914e18450a6beb
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-29 06:51:28 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-29

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/SampleGame1 (Beginner)/Scripts/Util/GameEvents.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/SampleGame1 (Beginner)/Scripts/Util/GameEvents.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 января 2026 06:50:28
- * Version: 1.0.160
+ * Last Updated: 29 января 2026 06:50:28
+ * Version: 1.0.161
  */
 
 namespace Zenject.Asteroids
