6bf84355398e46d3311856fc1c4a3f488a422a9f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-01-30 06:51:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-01-30

--- Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Boot/BootInstaller.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Boot/BootInstaller.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 29 января 2026 06:50:28
- * Version: 1.0.161
+ * Last Updated: 30 января 2026 06:50:28
+ * Version: 1.0.162
  */
 
 using Zenject;
