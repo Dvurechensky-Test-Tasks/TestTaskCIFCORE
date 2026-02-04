cd014ba29eebea5bc79a361c7d22bba9db7c4bed
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-04 06:51:30 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-04

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/CancellableTaskCompletionSource.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/CancellableTaskCompletionSource.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 03 февраля 2026 06:50:28
- * Version: 1.0.166
+ * Last Updated: 04 февраля 2026 06:50:28
+ * Version: 1.0.167
  */
 
 #if CSHARP_7_OR_LATER || (UNITY_2018_3_OR_NEWER && (NET_STANDARD_2_0 || NET_4_6))
