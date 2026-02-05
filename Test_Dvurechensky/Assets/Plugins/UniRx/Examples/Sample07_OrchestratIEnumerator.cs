5b483492c9ff799d7a4cbda4211d36881d4d2e47
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-05 07:41:37 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-05

--- Test_Dvurechensky/Assets/Plugins/UniRx/Examples/Sample07_OrchestratIEnumerator.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Examples/Sample07_OrchestratIEnumerator.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 февраля 2026 06:50:28
- * Version: 1.0.167
+ * Last Updated: 05 февраля 2026 07:40:37
+ * Version: 1.0.168
  */
 
 #pragma warning disable 0168
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 февраля 2026 06:50:28
- * Version: 1.0.167
+ * Last Updated: 05 февраля 2026 07:40:37
+ * Version: 1.0.168
  */
 
 #if !(NETFX_CORE || NET_4_6 || NET_STANDARD_2_0 || UNITY_WSA_10_0)
