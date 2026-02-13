f0b4635ef9cd61c0f35f85ad713f710e1948f978
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-13 11:49:48 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-13

--- Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 12 февраля 2026 10:31:34
- * Version: 1.0.175
+ * Last Updated: 13 февраля 2026 11:48:56
+ * Version: 1.0.176
  */
 
 public class ForecastItemFactory
