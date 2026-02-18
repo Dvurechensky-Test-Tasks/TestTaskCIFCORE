14c82cca3388c4439c32d62759f626ff604158f3
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-18 13:23:35 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-18

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 февраля 2026 09:24:49
- * Version: 1.0.180
+ * Last Updated: 18 февраля 2026 13:22:35
+ * Version: 1.0.181
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
