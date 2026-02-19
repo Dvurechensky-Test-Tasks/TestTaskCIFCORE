773d14e2547d8656f4a52c25af7183413941837d
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-19 10:54:54 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-19

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 февраля 2026 13:22:35
- * Version: 1.0.181
+ * Last Updated: 19 февраля 2026 10:53:54
+ * Version: 1.0.182
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
+++ Test_Dvurechensky/Assets/_Scripts/Mono/Logic/Forecast/ForecastItem/ForecastItemFactory.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 февраля 2026 13:22:35
- * Version: 1.0.181
+ * Last Updated: 19 февраля 2026 10:53:54
+ * Version: 1.0.182
  */
 
 public class ForecastItemFactory
