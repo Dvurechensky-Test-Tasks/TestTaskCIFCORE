ade12240645b73eaab54ffc7b83e30d08314bfc2
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-02-22 13:31:35 +0300

    Auto-update headers via pre_commit.ps1 - 2026-02-22

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Disposables/RefCountDisposable.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Disposables/RefCountDisposable.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 февраля 2026 06:50:29
- * Version: 1.0.184
+ * Last Updated: 22 февраля 2026 13:30:34
+ * Version: 1.0.185
  */
 
 // This code is borrwed from Rx Official and some modified.
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 21 февраля 2026 06:50:29
- * Version: 1.0.184
+ * Last Updated: 22 февраля 2026 13:30:34
+ * Version: 1.0.185
  */
 
 #if !(NETFX_CORE || NET_4_6 || NET_STANDARD_2_0 || UNITY_WSA_10_0)
