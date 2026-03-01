9d69fb4255b74b3d8df19552a1832d84d87728ce
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-01 16:36:24 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-01

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 февраля 2026 15:54:06
- * Version: 1.0.191
+ * Last Updated: 01 марта 2026 16:35:26
+ * Version: 1.0.192
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/AutoLoader/Scene1Installer.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneContractTests/AutoLoader/Scene1Installer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 28 февраля 2026 15:54:06
- * Version: 1.0.191
+ * Last Updated: 01 марта 2026 16:35:26
+ * Version: 1.0.192
  */
 
 namespace Zenject.Tests.AutoLoadSceneTests
