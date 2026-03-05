76a766aa145875a413e75ae954886d1cde4f865f
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-05 14:58:26 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-05

--- Test_Dvurechensky/Assets/Plugins/Zenject/Source/Binding/Binders/Conventions/ConventionFilterTypesBinder.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/Source/Binding/Binders/Conventions/ConventionFilterTypesBinder.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 04 марта 2026 06:50:27
- * Version: 1.0.195
+ * Last Updated: 05 марта 2026 14:57:19
+ * Version: 1.0.196
  */
 
 #if !(UNITY_WSA && ENABLE_DOTNET)
