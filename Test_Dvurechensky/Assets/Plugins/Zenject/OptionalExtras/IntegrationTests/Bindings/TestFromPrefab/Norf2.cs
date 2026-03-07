b9ea1303d90bf1dc61849a55380453dd2b31a0f8
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-07 06:51:34 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-07

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Bindings/TestFromPrefab/Norf2.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/Bindings/TestFromPrefab/Norf2.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 06 марта 2026 06:50:29
- * Version: 1.0.197
+ * Last Updated: 07 марта 2026 06:50:28
+ * Version: 1.0.198
  */
 
 using UnityEngine;
