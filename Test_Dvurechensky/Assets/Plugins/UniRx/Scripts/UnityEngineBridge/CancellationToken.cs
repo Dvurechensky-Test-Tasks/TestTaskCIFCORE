3c13871eb7b0aadd87eccb71094633a92d6ed325
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-18 06:51:33 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-18

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/CancellationToken.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 17 марта 2026 06:50:29
- * Version: 1.0.207
+ * Last Updated: 18 марта 2026 06:50:28
+ * Version: 1.0.208
  */
 
 #if !(NETFX_CORE || NET_4_6 || NET_STANDARD_2_0 || UNITY_WSA_10_0)
