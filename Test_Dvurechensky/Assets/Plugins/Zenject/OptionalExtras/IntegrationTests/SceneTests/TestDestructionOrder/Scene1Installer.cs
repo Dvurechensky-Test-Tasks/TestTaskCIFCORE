65750316b94cc93b06b2cd6b1286909ba172e5ec
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-03-19 06:51:31 +0300

    Auto-update headers via pre_commit.ps1 - 2026-03-19

--- Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneTests/TestDestructionOrder/Scene1Installer.cs
+++ Test_Dvurechensky/Assets/Plugins/Zenject/OptionalExtras/IntegrationTests/SceneTests/TestDestructionOrder/Scene1Installer.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://sites.google.com/view/dvurechensky
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 18 марта 2026 06:50:28
- * Version: 1.0.208
+ * Last Updated: 19 марта 2026 06:50:28
+ * Version: 1.0.209
  */
 
 using UnityEngine.SceneManagement;
