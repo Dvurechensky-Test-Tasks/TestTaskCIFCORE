9ca93e5be6dc7ff27ddbe929efe4783e08a14bbc
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-10 12:31:14 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-10

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/ScheduledItem.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/InternalUtil/ScheduledItem.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 апреля 2026 10:55:50
- * Version: 1.0.231
+ * Last Updated: 10 апреля 2026 12:30:12
+ * Version: 1.0.232
  */
 
 // this code is borrowed from RxOfficial(rx.codeplex.com) and modified
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 09 апреля 2026 10:55:50
- * Version: 1.0.231
+ * Last Updated: 10 апреля 2026 12:30:12
+ * Version: 1.0.232
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
