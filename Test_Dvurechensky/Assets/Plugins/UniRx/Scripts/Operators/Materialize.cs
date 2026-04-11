98e75b73e82af7f4ced81c2576c90d5671619ccf
Dvurechensky <46356631+Dvurechensky@users.noreply.github.com>
2026-04-11 13:38:21 +0300

    Auto-update headers via pre_commit.ps1 - 2026-04-11

--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Operators/Materialize.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/Operators/Materialize.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 апреля 2026 12:30:12
- * Version: 1.0.232
+ * Last Updated: 11 апреля 2026 13:37:16
+ * Version: 1.0.233
  */
 
 using System;
--- Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
+++ Test_Dvurechensky/Assets/Plugins/UniRx/Scripts/UnityEngineBridge/Observable.Unity.cs
@@ -2,8 +2,8 @@
  * Author: Nikolay Dvurechensky
  * Site: https://dvurechensky.pro/
  * Gmail: dvurechenskysoft@gmail.com
- * Last Updated: 10 апреля 2026 12:30:12
- * Version: 1.0.232
+ * Last Updated: 11 апреля 2026 13:37:16
+ * Version: 1.0.233
  */
 
 #if !(UNITY_4_0 || UNITY_4_1 || UNITY_4_2 || UNITY_4_3 || UNITY_4_4 || UNITY_4_5 || UNITY_4_6 || UNITY_5_0 || UNITY_5_1 || UNITY_5_2)
